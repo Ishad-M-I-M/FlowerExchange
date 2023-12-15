1650076913192f20d188f2975b18d7012547317a
Ishad-M-I-M <ishad.19@cse.mrt.ac.lk>
2023-12-15 06:03:17 +0530

    Implement main function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <filesystem>
 
 #include "src/utils.h"
+#include "src/exchange_application.h"
+
+using namespace std;
 int main(int argc, char** argv) {
+    if (argc != 2){
+        cerr << "Invalid input. Command format: FlowerExchangeApp <order-csv>"<<endl;
+        exit(1);
+    }
     vector<utils::order> orders = utils::readOrderFile(argv[1]);
-    cout << "Hello" <<endl;
+    exchange_application exchange = exchange_application(orders);
+    vector<utils::execution> executions = exchange.get_executions();
+    utils::writeExecutionReport(executions, filesystem::current_path().string() + "/execution-report.csv");
 }
